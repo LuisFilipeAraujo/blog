f3706ddaaecc56e6f90f5c788ea0eac4ba41bb02
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-03-07 14:13:26 -0300

    loops - using fake objects to simulate the posts

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Luis'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username', 'Barry Allen'},
+            'body': 'Being the fastest man in the world is not easy!'
+        },
+        {
+            'author': {'username', 'John Stewart'},
+            'author': {'''In brightest day, in blackest night,
+                        No evil shall escape my sight.
+                        Let those who worship evil's might,
+                        Beware my power... Green Lantern's light! '''}
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, post=posts)
