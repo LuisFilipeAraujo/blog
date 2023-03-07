78aad907f0929d23477ec1dc6d7bdcc5419edcd5
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-03-07 14:57:09 -0300

    bugs fixed for the posts not showing

--- app/routes.py
+++ app/routes.py
@@ -8,11 +8,11 @@ def index():
     user = {'username': 'Luis'}
     posts = [
         {
-            'author': {'username', 'Barry Allen'},
+            'author': {'username': 'Barry Allen'},
             'body': 'Being the fastest man in the world is not easy!'
         },
         {
-            'author': {'username', 'John Stewart'},
+            'author': {'username': 'John Stewart'},
             'body': '''In brightest day, in blackest night,
                         No evil shall escape my sight.
                         Let those who worship evil's might,
